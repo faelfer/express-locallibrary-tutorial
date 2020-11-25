46fb4c065e9914ddcb4c793f17710461aa700928
faelfer <rfapp00@gmail.com>
2020-11-25 01:02:35 -0300

    :sparkles: bookInstance detail page

--- models/author.js
+++ models/author.js
@@ -19,19 +19,6 @@ AuthorSchema
   return this.family_name + ', ' + this.first_name;
 });
 
-// Virtual for author's lifespan
-AuthorSchema
-.virtual('lifespan')
-.get(function () {
-  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
-});
-
-// Virtual for author's URL
-AuthorSchema
-.virtual('url')
-.get(function () {
-  return '/catalog/author/' + this._id;
-});
 
 AuthorSchema
 .virtual('date_of_birth_formatted')
@@ -43,6 +30,20 @@ AuthorSchema
 .virtual('date_of_death_formatted')
 .get(function () {
   return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  return `${this.date_of_birth_formatted} - ${this.date_of_death_formatted}`
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
 });
 
 //Export model
